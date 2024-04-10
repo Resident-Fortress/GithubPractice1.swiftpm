028d870a7c5bab4da6dd991bd7f7bf5eb2ff2502
Austin Witt <awitt4481@stu.d214.org>
2024-04-10 14:54:23 -0500

    added number2 into contentview

--- ContentView.swift
+++ ContentView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var number1 = 120
+    @State var number2 = 60
     var body: some View {
         VStack {
             Image(systemName: "globe")
