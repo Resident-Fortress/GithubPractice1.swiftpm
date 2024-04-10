6d8b233e61fa7236d9d051e0aefa7a0bc735a771
Matthew Dudzinski <42253757+Resident-Fortress@users.noreply.github.com>
2024-04-10 14:56:39 -0500

    Created textview and added number2 intergration into it.

--- ContentView.swift
+++ ContentView.swift
@@ -4,11 +4,6 @@ struct ContentView: View {
     @State var number1 = 120
     @State var number2 = 60
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        Text("\(number2)")
     }
 }
